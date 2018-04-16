2cfd767261e2de8ca13089c8d075f1669a91b948
knromaric <nzekeng@gmail.com>
2018-04-16 10:35:02 +0300

    LoShuMagicSquare - Loop update

--- LoShuMagicSquare/LoShuMagicSquare.cs
+++ LoShuMagicSquare/LoShuMagicSquare.cs
@@ -28,11 +28,11 @@ namespace LoShuMagicSquare
         private static void DisplayBoard(int[,] numbers)
         {
             Console.WriteLine();
-            for (int i = 0; i < 3; i++)
+            for (int row = 0; row < 3; row++)
             {
-                for (int j = 0; j < 3; j++)
+                for (int column = 0; column < 3; column++)
                 {
-                    Console.Write(numbers[i, j] + " ");
+                    Console.Write(numbers[row, column] + " ");
                 }
                 Console.WriteLine();
             }
